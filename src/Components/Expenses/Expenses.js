e8e6c10a752b6b17f1c9cc79618de49716d0afb0
Pizhayut Watanasirichaikul <pizhayut_renz@hotmail.com>
2022-11-01 22:05:39 +0700

    Update

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 //import React from 'react'
 import "./Expenses.css";
-import Card from "./Card"
+import Card from "../UI/Card"
 import ExpenseItem from "./ExpenseItem";
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpenseItem
